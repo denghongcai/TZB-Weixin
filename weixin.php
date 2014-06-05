aefd5dcccfdab7b4374e95b7485d09f75779102c
chenlong1024 <361571243@qq.com>
2014-06-05 23:08:11 +0800

    nothing changed

--- weixin.php
+++ weixin.php
@@ -1,22 +1,14 @@
 <?php
 	define('BASEPATH', dirname(__FILE__));
 
-	require_once(BASEPATH . '/class/wechat.class.php');
-	require_once(BASEPATH . '/config.inc.php');
+	require(BASEPATH . '/class/wechat.class.php');
+	require(BASEPATH . '/config.inc.php');
 	
 	function __autoload($class_name)
 	{
-	    require_once BASEPATH . '/class/' . $class_name . '.class.php';
+	    require(BASEPATH . '/class/' . $class_name . '.class.php');
 	}
 
-	/*function logdebug($text){
-		file_put_contents('log/log.txt',$text."\n",FILE_APPEND);		
-	};
-	$options = array(
-		'token'=>'tokenaccesskey', //填写你设定的key
-		'debug'=>true,
-		'logcallback'=>'logdebug'
-	);*/
 	$options = $config['wx'];
 	$weObj = new Wechat($options);
 	
