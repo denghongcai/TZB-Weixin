a16be6d86b363b811825fc6191dc12fb57d86dcb
denghongcai <admin@dhchouse.com>
2014-07-24 14:35:01 +0800

    remove datatables.editor

--- application/views/adminContentList.php
+++ application/views/adminContentList.php
@@ -26,52 +26,7 @@
         </tfoot>
     </table>
     <script>
-        var editor;
         $(function(){
-            editor = new $.fn.dataTable.Editor({
-                ajax: "<?=base_url('content/UpdateContent')?>",
-                table: "#contentTables",
-                formOptions: {
-                },
-                fields: [
-                    {
-                        label: "标题",
-                        name: "Title"
-                    },
-                    {
-                        label: "作者",
-                        name: "Author"
-                    },
-                    {
-                        label: "内容",
-                        name: "Content"
-                    }
-                ],
-                i18n: {
-                    create: {
-                        button: "禁用",
-                        title:  "创建新条目",
-                        submit: "创建"
-                    },
-                    edit: {
-                        button: "修改",
-                        title:  "修改条目",
-                        submit: "确认修改"
-                    },
-                    remove: {
-                        button: "删除",
-                        title:  "删除",
-                        submit: "删除",
-                        confirm: {
-                            _: "你想删除这 %d 行?",
-                            1: "你想删除这一行吗？"
-                        }
-                    },
-                    error: {
-                        system: "系统出错，请联系系统管理员"
-                    }
-                }
-            });
             var ajaxUrl = "";
             var dt = undefined;
             var dtinit = function(cid){
@@ -82,7 +37,6 @@
                     dt.destroy();
 
                 dt = $('#contentTables').DataTable({
-                    'dom': 'Tfrtip',
                     'paging': false,
                     'lengthMenu': [[15, 25, 50, -1], [10, 25, 50, "全部"]],
                     'processing': true,
@@ -93,14 +47,6 @@
                         {'data': 'Author'},
                         {'data': 'Content'}
                     ],
-                    tableTools: {
-                        sRowSelect: "os",
-                        aButtons: [
-                            { sExtends: "editor_edit",   editor: editor },
-                            { sExtends: "editor_remove", editor: editor }
-                        ]
-                    },
-                    *
                     'language': {
                         'processing': '正在获取数据...',
                         'search': '搜索',
--- application/views/adminKnowledgeList.php
+++ application/views/adminKnowledgeList.php
@@ -20,52 +20,7 @@
     <script>
         var editor;
         $(function(){
-            editor = new $.fn.dataTable.Editor({
-                ajax: "<?=base_url('knowledge/UpdateKnowledge')?>",
-                table: "#knowTables",
-                formOptions: {
-                },
-                fields: [
-                    {
-                        label: "问题",
-                        name: "Question"
-                    },
-                    {
-                        label: "Tag",
-                        name: "Tag"
-                    },
-                    {
-                        label: "答案",
-                        name: "Answer"
-                    }
-                ],
-                i18n: {
-                    create: {
-                        button: "禁用",
-                        title:  "创建新条目",
-                        submit: "创建"
-                    },
-                    edit: {
-                        button: "修改",
-                        title:  "修改条目",
-                        submit: "确认修改"
-                    },
-                    remove: {
-                        button: "删除",
-                        title:  "删除",
-                        submit: "删除",
-                        confirm: {
-                            _: "你想删除这 %d 行?",
-                            1: "你想删除这一行吗？"
-                        }
-                    },
-                    error: {
-                        system: "系统出错，请联系系统管理员"
-                    }
-                }
-            });
             $('#knowTables').DataTable({
-                'dom': 'Tfrtip',
                 'lengthMenu': [[15, 25, 50, -1], [10, 25, 50, "全部"]],
                 'processing': true,
                 'serverSide': false,
@@ -75,13 +30,6 @@
                     {'data': 'Tag'},
                     {'data': 'Answer'}
                 ],
-                tableTools: {
-                    sRowSelect: "os",
-                    aButtons: [
-                        { sExtends: "editor_edit",   editor: editor },
-                        { sExtends: "editor_remove", editor: editor }
-                    ]
-                },
                 'language': {
                     'processing': '正在获取数据...',
                     'search': '搜索',
