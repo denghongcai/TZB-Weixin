7770ceb8b5c68fe50ea93cb5a254b85775780e79
chenlong <361571243@qq.com>
2014-08-16 13:30:53 +0800

    sae config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/TZB-Weixin-Admin/';
+$config['base_url']	= 'http://littlenut.sinaapp.com/';
 
 /*
 |--------------------------------------------------------------------------
