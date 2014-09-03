c8a71b1607ecd36390f0400cea5ac01a0452e804
chenlong <361571243@qq.com>
2014-09-03 23:51:58 +0800

    给标题添加链接

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -28,9 +28,9 @@ class Content extends MY_Controller {
                 $row['Edit'] = '<a href="'.base_url('content/UpdateContent/'.$cid).'">编辑</a>';
                 $row['Remove'] = '<a href="'.base_url('content/UpdateContent?action=remove&id='.$cid).'">删除</a>';
                 $row['DT_RowId'] = 'row_'.$cid;
+				$row['Content'] = mb_substr(strip_tags($row['Content']), 0, 80, 'UTF-8') . '…';
+				$row['Title'] = '<a href="http://tzb-weixin.dhc.house/content.php?id=' . $cid . '" target="view_window">' .  $row['Title'] . '</a>';
             }
-            $row['Content'] = mb_substr(strip_tags($row['Content']), 0, 80, 'UTF-8') . '…';
-            $row['Title'] = "<a href='http://tzb-weixin.dhc.house/content.php?id=$row[CONTENTID]'>$row[Title]</a>";
         }
         echo json_encode(
             array(
